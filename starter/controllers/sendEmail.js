8912b0017281d9a0c31d8d570c94a0e623584d39
ujangaja <ujangaja@gmail.com>
2021-12-01 15:41:15 +0700

    #Section 11: Send Emails | 228. Send Email With Ethereal

--- starter/controllers/sendEmail.js
+++ starter/controllers/sendEmail.js
@@ -1,5 +1,23 @@
+const nodemailer = require('nodemailer');
 const sendEmail = async (req, res) => {
-  res.send('send email func');
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'cole.jenkins99@ethereal.email',
+      pass: 'kSShShmn7CJjX4h2Mx',
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Ujang Arisandi" <ujangaja@gmail.com>',
+    to: 'arisandiujang@gmail.com',
+    subject: 'Hello',
+    html: '<h2>Sending Email with Node.js</h2>',
+  });
+  res.json(info);
 };
 
 module.exports = sendEmail;
